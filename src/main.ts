409be99c37df76beab75e86d57a749a1a1b69bdf
Gustavo Telles <gusstavotelles@outlook.com>
2022-03-10 17:46:35 +0000

    generate resources

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Desafio Conta Digital')
+    .setDescription('Desafio UME')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('conta-digital')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
